dbb617b2dfc4f273247d556fb9d96635b922794d
jnorstrom <johannes.norstrom@gmail.com>
2020-04-22 17:26:40 +0200

    indentering som nytt package upptäckte

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ print('Hello, ', name, '!')
 age = int(input("Enter your age: "))
 
 if age < 13:
-        print("It's great to be a kid")
-elif age in range(12,20):
-        print("You are a teenager!")
+    print("It's great to be a kid")
+elif age in range(12, 20):
+    print("You are a teenager!")
 else:
-        print("Time to grow UP!!")
+    print("Time to grow UP!!")
