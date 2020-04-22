81e4386efa0af9479f7def3a9ae6d6d79546b727
jnorstrom <64128147+jnorstrom@users.noreply.github.com>
2020-04-22 09:55:29 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,14 @@
+print("Hello World!")
+
+name = input('Enter your name: ')
+
+print('Hello, ', name, '!')
+
+age = int(input("Enter your age: "))
+
+if age < 13:
+        print("It's great to be a kid")
+elif age in range(12,20):
+        print("You are a teenager!")
+else:
+        print("Time to grow UP!!")
