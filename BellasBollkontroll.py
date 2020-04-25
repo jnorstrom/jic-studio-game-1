ede0813b5a732719e7b35c6ec0f8bc05536fac2e
isabelnorstrom <40183866+isabelnorstrom@users.noreply.github.com>
2020-04-25 22:10:32 +0200

    BellasBollkontroll 1.0

--- BellasBollkontroll.py
+++ BellasBollkontroll.py
@@ -1,6 +1,7 @@
 
 
 import turtle
+import os
 
 s = turtle.getscreen()
 
@@ -10,11 +11,15 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# Score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
 paddle_a.shape("square")
-paddle_a.color("green")
+paddle_a.color("white")
 paddle_a.shapesize(stretch_wid=5, stretch_len=1)
 paddle_a.penup()
 paddle_a.goto(-350, 0)
@@ -23,7 +28,7 @@ paddle_a.goto(-350, 0)
 paddle_b = turtle.Turtle()
 paddle_b.speed(0)
 paddle_b.shape("square")
-paddle_b.color("green")
+paddle_b.color("white")
 paddle_b.shapesize(stretch_wid=5, stretch_len=1)
 paddle_b.penup()
 paddle_b.goto(350, 0)
@@ -32,9 +37,29 @@ paddle_b.goto(350, 0)
 ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
-ball.color("red")
+ball.color("white")
+ball.shapesize(stretch_wid=1.5)
 ball.penup()
 ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0   |   Player B: 0", align="center", font=("Courier", 18, "normal"))
+
+# Mittenstreck
+line = turtle.Turtle()
+line.speed(0)
+line.color("white")
+line.penup()
+line.shapesize(stretch_wid=600, stretch_len=0.1)
+line.goto(0, 0)
 
 # Function
 def paddle_a_up():
@@ -42,18 +67,75 @@ def paddle_a_up():
     y += 20
     paddle_a.sety(y)
 
+
 def paddle_a_down():
     y = paddle_a.ycor()
     y -= 20
     paddle_a.sety(y)
 
+
+def paddle_b_up():
+    y = paddle_b.ycor()
+    y += 20
+    paddle_b.sety(y)
+
+
+def paddle_b_down():
+    y = paddle_b.ycor()
+    y -= 20
+    paddle_b.sety(y)
+
+
 # Keyboard binding
 wn.listen()
 wn.onkey(paddle_a_up, "w")
 wn.onkey(paddle_a_down, "s")
-
+wn.onkey(paddle_b_up, "Up")
+wn.onkey(paddle_b_down, "Down")
 
 
 # Main game loop
 while True:
     wn.update()
+
+    # Move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    # Border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+        #os.system("afplay bounce.wav&")
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+        #os.system("afplay bounce.wav&")
+
+    if ball.xcor() > 390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        #os.system("afplay goal.wav&")
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}   |   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 18, "normal"))
+
+    if ball.xcor() < -390:
+        ball.goto(0, 0)
+        ball.dx *= -1
+        score_b += 1
+        #os.system("afplay goal.wav&")
+        pen.clear()
+        pen.write("Player A: {}   |   Player B: {}".format(score_a, score_b), align="center", font=("Courier", 18, "normal"))
+
+    # Paddle and ball collisions
+    if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+        ball.setx(340)
+        ball.dx *= -1
+        #os.system("afplay bounce.wav&")
+
+    if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+        ball.setx(-340)
+        ball.dx *= -1
+        #os.system("afplay bounce.wav&")
